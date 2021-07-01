b99eaaa95409b7cf3ea99263b31d1ffc0438308e
Juan C Sanchez-Arias <43007095+juansamdphd@users.noreply.github.com>
2021-06-30 22:52:33 -0700

    Update Assg1

--- intro_to_r_workshop/IntroToR_Day1_Assg1.R
+++ intro_to_r_workshop/IntroToR_Day1_Assg1.R
@@ -1,4 +1,8 @@
-
+# Assignment #1 #####
+# Practice on your own!
+# 
+# Assignment_data1.csv
+# 
 ## set your working directory
 
 ## read in your data
@@ -12,12 +16,44 @@
 ##  with a title and nice, large enough labels 
 ##  using either base R or ggplot
 
-## if base R 
 
+# Load libraries ####
+library(tidyverse)
 
-## if ggplot 
+# Setting up working directory ####
+setwd("~/GitHub/bioinformatics.ca/intro_to_r_workshop")
 
+# Loading data ####
+df <- read.csv("Assignment_data1.csv", header = TRUE, as.is = TRUE)
 
+# Inspecting data ####
+str(df)
 
+# Creating responder_group variable ####
+df$responder_group <- factor(df$responder,
+                             levels = c(0, 1),
+                             labels = c("Non-responder", "Responder")
+                             )
 
+# Plotting data using ggplot2 #### 
+ggplot(df, aes(responder_group, biomarker)) +
+  stat_boxplot(geom = "errorbar", width = 0.25) +
+  geom_boxplot(aes(fill = responder_group)) +
+  theme_classic(base_size = 20) +
+  xlab("Responder Group") +
+  ylab("Biomarker (unit)") +
+  labs(title = "Boxplot using ggplot2") +
+  scale_fill_manual("Responder group", values = c("white", "grey"))
 
+# Plotting data using base R ####
+boxplot(df$biomarker ~ df$responder_group,
+        legend = c("Non-responder", "Responder"),
+        xlab = "Responder group",
+        ylab = "Biomarker (unit)",
+        main = "Boxplot using base R",
+        col = c("white", "grey"),
+        cex.lab = 1.5)
+legend(x = 1.8, y = 94,
+       legend = c("Control", "Case"),
+       fill = c("white", "grey"),
+       title = "Responder group")
